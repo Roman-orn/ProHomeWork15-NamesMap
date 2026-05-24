9cfef53006126d1d5c6bf69eed09e92faed82b85
Roman-orn <0671635566@ukr.net>
2026-05-24 20:31:14 +0300

    DataRepository fixing

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -1,36 +1,20 @@
 package app;
 
-import java.util.Map;
 import java.util.UUID;
 
 public class DataHandler {
 
-    private Map<UUID, String> data;
-    private int count;
+    private DataRepository dataRepository;
 
     public DataHandler(DataRepository dataRepository) {
-        this.data = dataRepository.getData();
+        this.dataRepository = new DataRepository();
     }
 
     public String getAll() {
-        if (data == null || data.isEmpty()) {
-            throw new IllegalArgumentException("No data!");
-        }
-
-        StringBuilder sb = new StringBuilder();
-        count = 1;
-        data.forEach((id, name) ->
-                sb.append(String.format("%d) %s %s%n",
-                        count++, id, name)));
-
-        return "ALL NAMES:\n" + sb;
+        return dataRepository.getData();
     }
 
     public String getById(UUID id) {
-        if (data.containsKey(id)) {
-            return String.format("NAME: id %s is %s", id, data.get(id));
-        } else {
-            return String.format("User with ID %s is not registered!", id);
-        }
+        return dataRepository.getUserById(id);
     }
 }
