d87069a2def255f6905248d8bc01a7bf5d3d77cc
Roman-orn <0671635566@ukr.net>
2026-05-24 19:22:38 +0300

    DataRepository refactoring

--- src/app/DataRepository.java
+++ src/app/DataRepository.java
@@ -2,17 +2,36 @@ package app;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class DataRepository {
 
-    Map<Integer, String> map = new HashMap<>();
+    private Map<UUID, String> usersByIdMap = new HashMap<>();
 
-    public Map<Integer, String> getData() {
-        map.put(25, "Lucy");
-        map.put(59, "Bob");
-        map.put(125, "Jack");
-        map.put(478, "Anna");
+    public DataRepository(){
+        User lucy = this.addUser("Lucy");
+        usersByIdMap.put(lucy.getId(lucy), lucy.getName(lucy));
+        User bob = this.addUser("Bob");
+        usersByIdMap.put(lucy.getId(lucy), lucy.getName(lucy));
+        User tom = this.addUser("Tom");
+        usersByIdMap.put(lucy.getId(lucy), lucy.getName(lucy));
+        User anna = this.addUser("Anna");
+        usersByIdMap.put(lucy.getId(lucy), lucy.getName(lucy));
+    }
 
-        return map;
+    public Map<UUID, String> getData() {
+        return usersByIdMap;
+    }
+
+    public User addUser(String name){
+        return new User(name);
+    }
+
+    public String getUserById(UUID id){
+        if(usersByIdMap.containsKey(id)){
+            return String.format("User with ID %s: %s", id, usersByIdMap.get(id));
+        } else {
+            throw new UserNotFoundException();
+        }
     }
 }
