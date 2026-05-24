7f84d55c47e670408a25bcfb77e050c854319eb9
Roman-orn <0671635566@ukr.net>
2026-05-24 11:53:54 +0300

    Added functionality to the Main class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,18 @@
 package app;
 
 public class Main {
+
+    public void main(String[] args) {
+
+        DataHandler handler = new DataHandler();
+        UIOperator operator = new UIOperator();
+
+        try {
+            operator.getOutput(handler.getAll());
+            operator.getOutput(handler.getById(25));
+        } catch (RuntimeException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
 }
