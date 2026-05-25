b1a5978819e86a57f4d68b6f94d6452d20f3dadb
Roman-orn <0671635566@ukr.net>
2026-05-25 23:16:03 +0300

    Fixing DataRepository class

--- src/app/DataRepository.java
+++ src/app/DataRepository.java
@@ -2,44 +2,40 @@ package app;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class DataRepository {
 
-    private Map<UUID, String> usersByIdMap = new HashMap<>();
-    private int count;
-
-    public DataRepository(){
-        usersByIdMap.put(UUID.fromString("4b0f6211-0935-4359-974b-b4c6bf62c11d"), "Lucy");
-        usersByIdMap.put(UUID.fromString("a84152c0-e1a4-464f-921d-4114b6dec5a9"), "Bob");
-        usersByIdMap.put(UUID.fromString("c8bdd952-151c-4484-8e0e-a29df2013333"), "Tom");
-        usersByIdMap.put(UUID.fromString("7446a500-af98-4573-9f3e-e7a1505f14ea"), "Anna");
-    }
+    private Map<Integer, User> usersByIdMap = new HashMap<>();
+    private int key;
 
     public String getData() {
         if (usersByIdMap == null || usersByIdMap.isEmpty()) {
-            throw new IllegalArgumentException("No data!");
+            throw new IllegalStateException("No data!");
         }
 
         StringBuilder sb = new StringBuilder();
-        count = 1;
-        usersByIdMap.forEach((id, name) ->
+        AtomicInteger count = new AtomicInteger(1);
+        usersByIdMap.forEach((key, user) ->
                 sb.append(String.format("%d) %s - %s%n",
-                        count++, id, name)));
+                        count.getAndIncrement(), key, user.getName())));
 
         return "ALL NAMES:\n" + sb;
     }
 
-    public void addUser(String name){
-        User user = new User(name);
-        usersByIdMap.put(user.getId(), user.getName());
+    public void addUser(String name, Long id){
+        User user = new User(name, id);
+        usersByIdMap.put(++key, user);
     }
 
-    public String getUserById(UUID id){
-        if(usersByIdMap.containsKey(id)){
-            return String.format("User with ID %s: %s", id, usersByIdMap.get(id));
+    public String getUserByKey(int key){
+        if(usersByIdMap.containsKey(key)){
+            return String.format("User with key %s: %s %d",
+                    key,
+                    usersByIdMap.get(key).getName(),
+                    usersByIdMap.get(key).getId());
         } else {
-            throw new UserNotFoundException(String.format("User with ID %s: Not found!", id));
+            throw new UserNotFoundException(String.format("User with key %s: Not found!", key));
         }
     }
 }
